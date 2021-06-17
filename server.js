383754d04f566979872b6b5541cc38fa89f1a70c
Cleyton Mendes <cleyton.mendest@gmail.com>
2021-06-17 00:34:05 -0300

    commit projeto agenda

---
+++ server.js
@@ -0,0 +1,53 @@
+require('dotenv').config(); //chama o dotenv (dep de segurança para ajudar a proteger senha e login do BD )
+const express = require('express'); // chama o express
+const app = express(); // express
+const mongoose = require('mongoose');
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.emit('pronto');
+  })
+  .catch(e => console.log(e));
+const session = require('express-session');// sessions para flash
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');// flash para mensagens autodestr
+const routes = require('./routes');// chama rotas
+const path = require('path');// chama path
+const helmet = require('helmet');// chama helmet (dep de segurança)
+const csrf = require('csurf');
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
+
+app.use(helmet());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOptions = session({
+  secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true
+  }
+});
+app.use(sessionOptions);
+app.use(flash());
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(csrf());
+// Nossos próprios middlewares
+app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
+app.use(routes);
+
+app.on('pronto', () => {
+  app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+  });
+});
---
+++ src/models/LoginModel.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcryptjs = require('bcryptjs');
+
+const LoginSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true }
+});
+
+const LoginModel = mongoose.model('Login', LoginSchema);
+
+class Login {
+    constructor(body){
+        this.body = body;
+        this.errors = [];
+        this.user = null;
+    }
+
+    async register(){
+        this.valida();
+        if(this.errors.length > 0)return;
+
+        await this.userExists();
+
+        if(this.errors.length > 0)return;
+
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
+        try{
+            this.user = await LoginModel.create(this.body);
+        } catch(e){
+            console.log(e);
+        }
+        
+    }
+
+    async userExists(){
+        const user = await LoginModel.findOne({ email: this.body.email });
+        if(user) this.errors.push('Usuário já existe.');
+    }
+
+    valida(){
+        this.cleanUp();
+        //Valida Campos
+        // E-mail válido
+        if(!validator.isEmail(this.body.email)) this.errors.push('E-mail Inválido');
+        // Senha entre 3 e 20 caracters
+        if(this.body.password.length < 3 || this.body.password.length > 20) this.errors.push('A senha precisa ter entre 3 e 20 caracteres');
+    }
+    cleanUp(){
+        for(const key in this.body){
+            if(typeof this.body[key] !== 'string'){
+                this.body[key] = '';
+            }
+        }
+
+        this.body = {
+            email: this.body.email,
+            password: this.body.password
+        }
+    }
+}
+
+module.exports = Login;
